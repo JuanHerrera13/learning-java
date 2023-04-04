62e3d714c338afecfde8d68ec8d9416878ba342f
Herrera, Juan (ESI) <juan.herrera@adp.com>
2023-04-04 13:22:13 -0300

    - Java studies: chained loop

---
+++ src/LoopsFlows/ChainedLoops.java
@@ -0,0 +1,13 @@
+package LoopsFlows;
+
+public class ChainedLoops {
+    public static void main(String[] args) {
+        for (int multiplier = 1; multiplier <= 10; multiplier++) {
+            for (int counter = 0; counter <= 10; counter++) {
+                System.out.print(multiplier * counter);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
